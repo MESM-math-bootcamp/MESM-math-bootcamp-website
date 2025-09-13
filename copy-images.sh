68467f697be68716495a5709b82696682a0626c6
carmengg <galaz-garcia@nceas.ucsb.edu>
2025-09-13 15:33:19 -0700

    Small updates to files.

--- copy-images.sh
+++ copy-images.sh
@@ -4,35 +4,19 @@
 SOURCE_FOLDER="/Users/c_galazgarcia/Documents/MESM-math-bootcamp/bren-math-workshop/lectures/img"
 
 # Define the destination folder in your project
-DEST_FOLDER="/Users/c_galazgarcia/Documents/MESM-math-bootcamp/MESM-math-bootcamp-website/notes/day2_limits/images"
+DEST_FOLDER="/Users/c_galazgarcia/Documents/MESM-math-bootcamp/MESM-math-bootcamp-website/notes/day3_derivatives/images"
 
 # Create the destination folder if it doesn't exist
 mkdir -p "$DEST_FOLDER"
 
 # List of image files to copy
 IMAGES=(
-  "cake.png"
-  "dsqrt.PNG"
-  "para.PNG"
-  "circle.png"
-  "cont.png"
-  "discont.png"
-  "tan.png"
-  "lim4.PNG"
-  "lim_graph_1.PNG"
-  "limx2.PNG"
-  "limit.gif"
-  "lim_graph_2.PNG"
-  "lim3.PNG"
-  "lim_graph_3.png"
-  "lim_g_a.PNG"
-  "lim_g_b.PNG"
-  "lim_g_c.PNG"
-  "tangent_line.png"
-  "loa.PNG"
-  "glacier.jpg"
-  "energy.png"
-  "bait.jpeg"
+  "exp_comic.png"
+  "nth.png"
+  "carry.webp"
+  "dochy.PNG"
+  "population-regions-with-projections.png"
+  "increase.PNG"
 )
 
 # Copy each image from the source folder to the destination folder
